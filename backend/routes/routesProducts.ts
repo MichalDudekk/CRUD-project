8a0a9d395835b333920d84a5c53cd1f8bbeeac4e
Michal Dudek <michaldudek.contact@gmail.com>
2026-01-09 15:58:10 +0100

    Add reviews endpoints

--- backend/routes/routesProducts.ts
+++ backend/routes/routesProducts.ts
@@ -3,6 +3,7 @@ import { Router, type Request, type Response } from "express";
 import { Op, type WhereOptions } from "sequelize";
 import authToken from "../middleware/authToken.js";
 import { User, Review, Product, Category } from "../models/index.js";
+import { error } from "console";
 
 const router = Router();
 
@@ -227,6 +228,144 @@ router.post(
         res.status(201).json({
             message: `Succesfully created category ${req.body.CategoryName}`,
         });
+    }
+);
+
+// Reviews
+
+router.get(
+    "/products/reviews/:ProductID",
+    async (req: Request, res: Response) => {
+        const reviews = await Review.findAll({
+            where: { ProductID: req.params.ProductID },
+        });
+        return res.status(200).json(reviews);
+    }
+);
+
+interface CreateReviewBody {
+    ProductID: number;
+    Rating: number;
+    Content: string;
+    Status: string;
+}
+
+router.post(
+    "/products/reviews",
+    authToken,
+    async (req: Request<{}, {}, CreateReviewBody>, res: Response) => {
+        const user = res.locals.user;
+
+        if (!user) {
+            console.log("user missing in locals");
+            return res.status(500).json({ error: "Server Error" });
+        }
+
+        try {
+            const { ProductID, Rating, Content, Status } = req.body;
+
+            const product = await Product.findByPk(ProductID);
+            if (!product)
+                return res.status(404).json({ error: "Product not found" });
+
+            await Review.create({
+                ProductID,
+                Rating: Number(Rating),
+                Content,
+                Status,
+                UserID: user.UserID,
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: "Server Error" });
+        }
+
+        res.status(201).json({
+            message: `Succesfully created review`,
+        });
+    }
+);
+
+interface UpdateReviewBody {
+    ReviewID: number;
+    Rating: number;
+    Content: string;
+}
+
+router.patch(
+    "/products/reviews",
+    authToken,
+    async (req: Request<{}, {}, UpdateReviewBody>, res: Response) => {
+        const user = res.locals.user;
+
+        if (!user) {
+            console.log("user missing in locals");
+            return res.status(500).json({ error: "Server Error" });
+        }
+
+        try {
+            const { Rating, Content, ReviewID } = req.body;
+
+            const review = await Review.findByPk(ReviewID);
+
+            if (!review) {
+                return res.status(404).json({ error: "Review not found" });
+            }
+
+            if (review.UserID !== user.UserID) {
+                return res
+                    .status(403)
+                    .json({ error: "Access denied: You are not the author" });
+            }
+
+            await review.update({
+                Rating: Rating,
+                Content: Content,
+            });
+
+            return res.status(200).json({
+                message: "Successfully updated review",
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: "Server Error" });
+        }
+    }
+);
+
+router.delete(
+    "/products/reviews/:ReviewID",
+    authToken,
+    async (req: Request, res: Response) => {
+        const user = res.locals.user;
+
+        if (!user) {
+            console.log("user missing in locals");
+            return res.status(500).json({ error: "Server Error" });
+        }
+
+        try {
+            const ReviewID = Number(req.params.ReviewID);
+            const review = await Review.findByPk(ReviewID);
+
+            if (!review) {
+                return res.status(404).json({ error: "Review not found" });
+            }
+
+            if (!user.IsAdmin && review.UserID !== user.UserID) {
+                console.log("you need to be admin or author");
+                return res.status(403).json({ error: "Access denied" });
+            }
+
+            await review.destroy();
+
+            return res.status(200).json({
+                message: "Successfully deleted review",
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: "Server Error" });
+        }
     }
 );
 
