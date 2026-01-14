ed61cbd14ac1603e06b244e4d637c9691d2bdd84
Michal Dudek <michaldudek.contact@gmail.com>
2026-01-14 02:06:53 +0100

    Add dockerfile for backend and docker-compose for whole app

--- backend/database.ts
+++ backend/database.ts
@@ -1,9 +1,9 @@
 // database.ts
 import { Sequelize } from "sequelize";
-import { DB_NAME, DB_USER, DB_PASSWORD } from "./config/env.js";
+import { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } from "./config/env.js";
 
 const database = new Sequelize(DB_NAME!, DB_USER!, DB_PASSWORD!, {
-    host: "127.0.0.1",
+    host: DB_HOST,
     dialect: "postgres",
     logging: false, // Wyłącza logi SQL w konsoli
 });
